77cac198ef31a493f406080031ea7d98ffc93fba
Raphael Kaviak <rkm17@inf.ufpr.br>
2021-03-15 01:43:33 -0300

    add requirements e arrumando js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
 $('input[name ="password"], input[name ="password2"]').on('keyup', function () {
     if ($('input[name ="password"]').val() == $('input[name ="password2"]').val()) {
       $('#message').html('Senhas iguais').css('color', 'green');
-      $('button').prop('disabled',false);
+      // $('button').prop('disabled',false);
     } else{
       $('#message').html('As duas senhas não são iguais!').css('color', 'red');
-      $('button').prop('disabled',true);
+      // $('button').prop('disabled',true);
     }
 });
 
@@ -16,7 +16,6 @@ $.getJSON( "http://localhost:8000/users/all_users/", function( data ) {
       emails.push(data[i]["email"]);
     }
     
-    // $('input[name ="username"], input[name ="email"], input[name ="phone"]').on('select', function () {
     $('.form-group').on('select', function () {  
       if (usernames.includes($('input[name ="username"]').val())) {
         $('#message').html('Este Nome de usuário já existe! Escolha outro').css('color', 'red');
